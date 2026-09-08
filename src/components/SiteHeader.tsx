10303be1bf0fb8c6226c569f7098085acbec14de
mshanarh <ntobekotraders.official@gmail.com>
2026-09-09 00:38:10 +0200

    Use uploaded platform logo asset

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -20,7 +20,7 @@ export function SiteHeader() {
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-5">
         <Link to="/" className="flex items-center gap-2">
           <img
-            src="/ea-migrate-platform-robot.png"
+            src="/ea-migrate-platform-robot.jpg"
             alt="EA Migrate Pro"
             className="size-10 object-contain"
           />
